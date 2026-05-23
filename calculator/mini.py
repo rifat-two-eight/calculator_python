71e257809b0e936ed01d42e5d8fc9f761e9a5d3e
rifat-two-eight <rifat28.dev@gmail.com>
2026-05-24 03:22:04 +0600

    first commit

---
+++ calculator/mini.py
@@ -0,0 +1,18 @@
+number1 = float(input("enter number1 : "))
+number2 = float(input("enter number2 : "))
+
+additon = (number1+number2)
+
+substraction = (number1-number2)
+
+multiplication = (number1*number2)
+
+division = (number1/number2)
+
+print("jogfol = ",additon)
+
+print("biyogfol = ",substraction)
+
+print("gunfol = ",multiplication)
+
+print("vagfol = ",division)
